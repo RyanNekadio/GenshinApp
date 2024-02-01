bdfa673d70e441f319df82d0ec2ec6dd91361335
Ryan Nekadio <ryanekadio12@hotmail.co.uk>
2024-02-01 17:09:27 +0000

    Uploaded server skeleton for Genshin project

---
+++ GenshinApp/src/main/java/com/example/GenshinApp/GenshinAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.GenshinApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenshinAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenshinAppApplication.class, args);
+	}
+
+}
---
+++ GenshinApp/src/test/java/com/example/GenshinApp/GenshinAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GenshinApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenshinAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
